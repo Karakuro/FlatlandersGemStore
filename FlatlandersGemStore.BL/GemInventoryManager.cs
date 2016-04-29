02c43a86d23d19e64001715bb3c6e801501ca085
Domenico Sironi <domy_siro@hotmail.com>
2016-04-29 15:57:27 +0200

    Solution Aggiornata per Percorso

---
+++ FlatlandersGemStore.BL/GemInventoryManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlatlandersGemStore.Entities;
+using FlatlandersGemStore.Entities.Entities;
+
+
+namespace FlatlandersGemStore.BL
+{
+    public class GemInventoryManager
+    {
+        private List<Gem> Inventory;
+        public GemInventoryManager()
+        {
+            Inventory = new List<Gem>();
+            Inventory.AddRange(StoreWarehouse.Gems);
+        }
+
+        public void StoreNewGems()
+        {
+            Inventory.AddRange(StoreWarehouse.Gems);
+        }
+
+        public Gem GetSingleGem(string name)
+        {
+            return Inventory.FirstOrDefault(x => x.Name == name);
+        }
+
+        public bool SellSingleGem(string name)
+        {
+            return Inventory.Remove(GetSingleGem(name));
+        }
+
+        public List<Gem> GetAllInventory()
+        {
+            return Inventory;
+        }
+    }
+}
---
+++ FlatlandersGemStore/Controllers/Api/GemInventoryController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Results;
+using System.Web.Http.Description;
+using FlatlandersGemStore.Entities.Entities;
+using FlatlandersGemStore.BL;
+
+namespace FlatlandersGemStore.Controllers.Api
+{
+    public class GemInventoryController : ApiController
+    {
+        public GemInventoryManager manager;
+        /// <summary>
+        /// Richiede la lista completa delle gemme in vendita
+        /// </summary>
+        /// <returns></returns>
+        public List<Gem> Get()
+        {
+            try
+            {
+                if (manager == null)
+                    manager = new GemInventoryManager();
+                return manager.GetAllInventory();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// richiede i dettagli di una singola gemma
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Gem Get(string id)
+        {
+            try
+            {
+                if (manager == null)
+                    manager = new GemInventoryManager();
+                return manager.GetSingleGem(id);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Registra l'arrivo di un nuovo carico di gemme
+        /// </summary>
+        /// <returns></returns>
+        public bool Post()
+        {
+            try
+            {
+                if (manager == null)
+                    manager = new GemInventoryManager();
+                manager.StoreNewGems();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Vende (rimuovendola dall'inventario) una singola gemma
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public bool Post(string id)
+        {
+            try
+            {
+                if (manager == null)
+                    manager = new GemInventoryManager();
+                return manager.SellSingleGem(id);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
